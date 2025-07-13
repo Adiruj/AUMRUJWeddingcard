cba63aff5a9bad0b11a0a011403d772f8fdaa3f6
Adiruj <adiruj-s@hotmail.com>
2025-07-13 20:46:03 +0700

    Fix Gallery

--- components/garllery.tsx
+++ components/garllery.tsx
@@ -3,53 +3,52 @@ import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 export default function GalleryCarousel() {
-
   const images = [
-    "/gallery/01.jpeg",
-    "/gallery/02.jpeg",
-    "/gallery/03.jpeg",
-    "/gallery/04.jpeg",
-    "/gallery/05.jpeg",
-    "/gallery/06.jpg",
-    "/gallery/07.jpeg",
-    "/gallery/08.jpg",
-    "/gallery/09.jpg",
-    "/gallery/10.jpg",
-    "/gallery/11.jpg",
-    "/gallery/12.jpg",
-    "/gallery/13.jpg",
-    "/gallery/14.jpg",
-    "/gallery/15.jpg",
-    "/gallery/16.jpg",
-    "/gallery/17.jpg",
-    "/gallery/18.jpg",
-    "/gallery/19.jpg",
-    "/gallery/20.jpg",
-    "/gallery/21.jpg",
-    "/gallery/22.jpg",
-    "/gallery/23.jpg",
-    "/gallery/24.jpg",
+    "/gallery/01.JPEG",
+    "/gallery/02.JPEG",
+    "/gallery/03.JPEG",
+    "/gallery/04.JPEG",
+    "/gallery/05.JPEG",
+    "/gallery/06.JPG",
+    "/gallery/07.JPEG",
+    "/gallery/08.JPG",
+    "/gallery/09.JPG",
+    "/gallery/10.JPG",
+    "/gallery/11.JPG",
+    "/gallery/12.JPG",
+    "/gallery/13.JPG",
+    "/gallery/14.JPG",
+    "/gallery/15.JPG",
+    "/gallery/16.JPG",
+    "/gallery/17.JPG",
+    "/gallery/18.JPG",
+    "/gallery/19.JPG",
+    "/gallery/20.JPG",
+    "/gallery/21.JPG",
+    "/gallery/22.JPG",
+    "/gallery/23.JPG",
+    "/gallery/24.JPG",
   ];
 
   return (
     <div className="w-full max-w-screen-md mx-auto">
       <Carousel
-        showArrows={true}
-        showThumbs={false}
-        showStatus={false}
-        infiniteLoop={true}
         autoPlay={true}
-        interval={4000}
-        swipeable={true}
-        emulateTouch={true}
         dynamicHeight={false}
+        emulateTouch={true}
+        infiniteLoop={true}
+        interval={4000}
+        showArrows={true}
+        showStatus={false}
+        showThumbs={false}
+        swipeable={true}
       >
         {images.map((src, index) => (
           <div key={index} className="w-full">
             <img
               src={src}
-              alt={`img-${index}`}
               className="w-full h-auto max-h-[500px] object-contain rounded-xl"
+              alt={`img-${index}`}
             />
           </div>
         ))}
